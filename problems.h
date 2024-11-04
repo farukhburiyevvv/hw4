e0363d17db130dd35da5633862d52ae996cd37b7
farrukh <f.buriyev@newuu.uz>
2024-11-04 14:41:11 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,3 +1,9 @@
+//In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
+//DO NOT CHANGE FUNCTIONS' NAMES.
+//You may check your functions in main.cpp as shown by exampleFunction;
+void exampleFunction() {
+    std::cout<<"Example Function that can be called in main."<<std::endl;
+}
 // Problem 1
 int floorFunction(float a){
   return a;

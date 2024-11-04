c045c5d387c032e4b8b0c6b67c5ce27aedd69d34
farrukh <f.buriyev@newuu.uz>
2024-11-04 14:23:05 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -55,7 +55,7 @@ int decimalToBinary(int n) {
 
 // Problem 6
 int midValue(int n1, int n2, int n3){
-    if(n1>=n2 && n1<=n3) return n1;
-    else if(n2>=n1 && n2<=n3) return n2;
+    if(n1>n2 && n1<n3) return n1;
+    else if(n2>n1 && n2<n3) return n2;
     return n3;
 }

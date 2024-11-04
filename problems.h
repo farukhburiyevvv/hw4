65b54922581c9d0e4239c8303df88db52728c947
farrukh <f.buriyev@newuu.uz>
2024-11-04 14:34:32 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,18 +1,16 @@
-
+#include <bits/stdc++.h>
 // Problem 1
-int floorFunction(float a){
-  return a;
+//floorFunction
+//ceilingFunction
+int floorFunction(float num) {
+    return static_cast<int>(num);
 }
-
-int ceilingFunction(float a){
-    int x = int (a);
-    if(x-a==0){
-        return a;
-    }
-    return a+1;
+int ceilingFunction(float num) {
+    int p = static_cast<int>(num);
+    return (p < num ? p+1 : p);
 }
-
 // Problem 2
+//swap_values
 template<typename T>
 void swap_values(T& x, T& y) {
     T temp = x;
@@ -20,35 +18,41 @@ void swap_values(T& x, T& y) {
     y=temp;
 }
 // Problem 3
+//multiply
 int multiply(int a, int b) {
-    if (b == 0) {
+    if (b == 0  a==0) {
         return 0;
     }
-    if (b < 0) {
-        return -multiply(a, -b);
-    }
-    return a + multiply(a, b - 1);
+    if (b>0)
+    return a+multiply(a, b-1);
+    return -multiply(a, -b);
 }
-
 // Problem 4
-int digitSum(int a) {
-    int s=0;
-    if(a>0){
-        s+=a%10;
-        return s+digitSum(a/10);
-    }
-    return 0;
-}
+//digitSum
+int digitSum(int n) {
+    int sum = 0;
+    if (n == 0) {
+        return 0;
+    }return sum+=n%10+digitSum(n/10);
 
+
+}
 // Problem 5
+//decimalToBinary
 int decimalToBinary(int n) {
-    if (n <= 0) return 0; 
-    int binary = decimalToBinary(n / 2);
-    return binary * 10 + (n % 2);
+    int t=log2(n)/1;
+    int res=0;
+    for(int i=t;i>=0;i--) {
+        if(pow(2,i)<=n) {
+            res+=1*pow(10,i);
+            n-=pow(2,i);
+        }
+
+    }return res;
+
 }
 
 // Problem 6
-
 //midValue
 template <typename T> T midValue (T a, T b,T c) {
     if((a>b && b>c)  (c>b && b>a)) {
